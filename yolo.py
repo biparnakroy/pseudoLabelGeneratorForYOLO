503e89e3c1382fc1c508bdef25cf67d442a75f8c
Biparnak Roy <54219771+biparnakroy@users.noreply.github.com>
2020-07-06 10:49:14 +0530

    Update yolo.py

--- yolo.py
+++ yolo.py
@@ -4,11 +4,14 @@ import argparse
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help="path to input image")
 ap.add_argument("-t", "--text", required=True, help="path to input label")
+ap.add_argument("-c", "--classes", required=True, help="list of classes")
 args = vars(ap.parse_args())
 
+classes = []
+with open(args["classes"], "r") as x:
+    classes = [line.strip() for line in x.readlines()]
+
 filename = args["text"]
-
-
 img = cv2.imread(args["image"])
 
 height=int(img.shape[0])
@@ -18,17 +21,16 @@ width = int(img.shape[1])
 
 with open(filename) as f:
     content = f.readlines()
-# you may also want to remove whitespace characters like `\n` at the end of each line
+
 content = [x.strip() for x in content] 
 
 if len(content)>0:
     for i in content:
         if (int(i.split()[5]) > 0 and int(i.split()[3]) > 0):
             l=""
-            if i.split()[0]=="mask:":
-                l=l+"1 "
-            else:
-                l=l+"0 "
+            for c in classes:    
+                l=l+str(classes.index(i.split()[0][:len(i.split()[0])-1]))
+
             Ax=int(i.split()[3])+(int(i.split()[7])/2)
             x= int(Ax)/width
             l=l+str("%.6f"%x)+" "
