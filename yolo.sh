9fbf0691e235011fae529109e0ecf7393f0060c7
Biparnak Roy <roybiparnak.com>
2020-07-06 19:26:37 +0530

    dataset

--- yolo.sh
+++ yolo.sh
@@ -1,5 +1,5 @@
-mkdir vic
+mkdir labels
 for i in  1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
 do
-  python3 yolo.py --image tamodip2/mox$i.jpg --text tamodip2/mox$i.txt > vic/mox$i.txt
+  python3 yolo.py --image dataset/data$i.jpg --text dataset/data$i.txt > labels/mox$i.txt
 done
