70ddd664cd958910b60eff7d2a1a61fdfc48f91a
Biparnak Roy <roybiparnak@gmail.com>
2020-08-14 09:24:12 +0530

    Update yolo.sh

--- yolo.sh
+++ yolo.sh
@@ -1,4 +1,4 @@
 for i in  1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
 do
-  python3 yolo.py --image dataset/data$i.jpg --text dataset/data$i.txt > dataset/data$i.txt
+  python3 yolo.py --image dataset/data$i.jpg --text dataset/data$i.txt -classes classes.txt > dataset/data$i.txt
 done
